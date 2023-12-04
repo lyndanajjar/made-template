0d65c36fd36bf74b8a763d641b5f6a4dde0f1e85
lyndanajjar <lyndanajjar15@gmail.com>
2023-12-05 00:36:04 +0100

    Add automated tests.

---
+++ project/tests.py
@@ -0,0 +1,134 @@
+import unittest
+import subprocess
+import os
+import sqlite3
+
+class TestDataPipeline(unittest.TestCase):
+
+    def setUp(self):
+        self.db_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'Linda_data.db')
+
+    def test_data_pipeline(self):
+
+
+        # Check if the SQLite database is created
+        self.assertTrue(os.path.exists(self.db_path))
+
+        # Check if the necessary tables exist in the database
+        self.assertTrue(self.table_exists('startup_data'))
+        self.assertTrue(self.table_exists('germany_combined_gdp'))
+        self.assertTrue(self.table_exists('startup_data_business'))
+        self.assertTrue(self.table_exists('employment_growth_data'))
+        self.assertTrue(self.table_exists('insolvency_data'))
+
+        # Check data consistency and completeness
+        self.test_startup_data_consistency()
+        self.test_gdp_growth_consistency()
+        self.test_insolvency_rates_consistency()
+        self.test_total_employment_growth_consistency()
+        self.test_business_startups_consistency()
+
+    def table_exists(self, table_name):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';")
+        result = cursor.fetchone()
+        conn.close()
+        return result is not None
+    
+    
+    
+    
+    def get_row_count(self, table_name):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT COUNT(*) FROM {table_name};")
+        result = cursor.fetchone()[0]
+        conn.close()
+        return result
+    
+    
+    def check_no_null_values(self, table_name, columns):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+
+        for column in columns:
+        # escape column names with spaces
+            escaped_column = f'"{column}"' if ' ' in column else column
+            query = f"DELETE FROM {table_name} WHERE {escaped_column} IS NULL;"
+            cursor.execute(query)
+
+        conn.commit()
+        conn.close()
+        
+
+        
+        
+    def check_negative_insolvency_rates(self):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+
+        query = "SELECT COUNT(*) FROM insolvency_data WHERE Insolvencies < 0;"
+        cursor.execute(query)
+        negative_values_count = cursor.fetchone()[0]
+        self.assertEqual(negative_values_count, 0, "Negative values found in Insolvencies.")
+
+        conn.close()   
+
+    def test_startup_data_consistency(self):
+        expected_startup_data_row_count = 32        
+        actual_row_count = self.get_row_count('startup_data')
+        #print(f"Actual Row Count for startup_data: {actual_row_count}")
+        self.assertEqual(actual_row_count, expected_startup_data_row_count)
+        self.check_no_null_values('startup_data', ['Bundesland','Typ','Startups'])  
+        
+
+
+    def test_gdp_growth_consistency(self):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM germany_combined_gdp;")
+        data = cursor.fetchall()
+        conn.close()
+
+    # Extract the 'GDP per Capita (Current US Dollars)' column
+        gdp_per_capita_column = [row[2] for row in data]  # Assuming 'GDP per Capita' is the third column (index 2)
+
+    # Check that all values in the 'GDP per Capita' column are positive
+        self.assertTrue(all(gdp_per_capita is not None and gdp_per_capita > 0 for gdp_per_capita in gdp_per_capita_column if gdp_per_capita is not None))
+
+    # Check that there are no null values 
+        self.check_no_null_values('germany_combined_gdp', ['Year', 'GDP Growth Rate', 'GDP per Capita (Current US Dollars)'])
+
+
+
+    def test_insolvency_rates_consistency(self):
+        expected_insolvency_rates_row_count = 23
+        self.assertEqual(self.get_row_count('insolvency_data'), expected_insolvency_rates_row_count)
+        self.check_no_null_values('insolvency_data', ['Year','Insolvencies'])  
+
+
+
+    def test_total_employment_growth_consistency(self):
+        expected_employment_growth_row_count = 31
+        self.assertEqual(self.get_row_count('employment_growth_data'), expected_employment_growth_row_count)
+        self.check_no_null_values('employment_growth_data', ['Year','Total','Agriculture, Forestry, and Fishing','Manufacturing Industry excl. Construction','Construction','Service Industries'])  
+    
+      
+
+    def test_business_startups_consistency(self):
+        expected_startup_business_row_count = 20
+        self.assertEqual(self.get_row_count('startup_data_business'), expected_startup_business_row_count)
+        self.check_no_null_values('startup_data_business', ['Year','Business Startups'])  
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
